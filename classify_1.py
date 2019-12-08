9054f46bc78dc9ef66a187e7356de0cedec187ca
Laura Sipiä <laura.sipia@tuni.fi>
2019-12-08 21:54:02 +0200

    Custom model

--- classify_1.py
+++ classify_1.py
@@ -6,7 +6,8 @@ Created on Fri Nov 22 22:46:54 2019
 """
 
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout, GlobalAveragePooling2D
+from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -14,48 +15,101 @@ from tensorflow.keras.utils import to_categorical
 from load_images import load_data
 import os
 
+IMG_SIZE = 64
+DATA_PATH="../data/train"
+BATCH_SIZE=32
+#%%
 #create model
 # Best test acc score 0.716 with 4000 samples where 0.2 are test samples
 # For 64 feature maps
 # 10/10 ---- loss: 0.5781 - acc: 0.7666 - val_loss: 0.6023 - val_acc: 0.7125
 model = Sequential()
 
-N = 64 # Number of feature maps
-w, h = 5, 5 # Conv. window size
+N_1 = 16 # Number of feature maps
+N_2 = 32 
+N_3 = 64 
+w, h = 3, 3 # Conv. window size
 
-model.add(Conv2D(N, (w, h), 
-                 input_shape=(64, 64, 3),
+model.add(Conv2D(N_1, (w, h), 
+                 input_shape=(IMG_SIZE, IMG_SIZE, 3),
                  activation = 'relu',
                  padding = 'same'))
 
-model.add(MaxPooling2D(pool_size=(4, 4)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(N, (w, h),
+model.add(Conv2D(N_2, (w, h),
                  activation = 'relu',
                  padding = 'same'))
 
-model.add(MaxPooling2D(pool_size=(4,4)))
+model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Flatten())
+model.add(Conv2D(N_3, (w, h),
+                 activation = 'relu',
+                 padding = 'same'))
 
-model.add(Dense(100, activation = 'softmax'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(N_3, (w, h),
+                 activation = 'relu',
+                 padding = 'same'))
+
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(GlobalAveragePooling2D())
+
+model.add(Dense(1024, activation = 'relu'))
+
+model.add(Dropout(0.2))
+
+model.add(Dense(512, activation = 'relu'))
+
+model.add(Dropout(0.4))
 
 model.add(Dense(2, activation = 'softmax'))
+
+model.summary()
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+#%%
 
-if os.path.exists("X.npy") and os.path.exists("y.npy"):
-    X = np.load("X.npy")
-    y = np.load("y.npy")
+aug_gen = ImageDataGenerator(
+        horizontal_flip=True,
+        samplewise_std_normalization=True,
+        validation_split=0.2
+        )
+
+train_gen = aug_gen.flow_from_directory(
+        directory=DATA_PATH,
+        target_size=(IMG_SIZE,IMG_SIZE),
+        batch_size=BATCH_SIZE,
+        class_mode="categorical",
+        shuffle=True,
+        subset="training" 
+        )
+
+validation_gen = aug_gen.flow_from_directory(
+        directory=DATA_PATH,
+        target_size=(IMG_SIZE, IMG_SIZE),
+        batch_size=BATCH_SIZE,
+        class_mode="categorical",
+        shuffle=False,
+        subset="validation"
+        )
+#%%
+#NOT USED
+"""
+if os.path.exists("X_color_64.npy") and os.path.exists("y_color_64.npy"):
+    X = np.load("X_color_64.npy")
+    y = np.load("y_color_64.npy")
 else:
-    X, y = load_data()
+    X, y = load_data(IMG_SIZE)
 
 print("X shape is: ", X.shape)
 X, y = shuffle(X,y)
-X = X[:4000, :,:,:] 
-y = y[:4000]
+#X = X[:4000, :,:,:] 
+#y = y[:4000]
 
 #Normalizing the data to scale 0-1
 print("Max on: ", X.max())
@@ -65,5 +119,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
 y_train = to_categorical(y_train) 
 y_test = to_categorical(y_test)
-
-model.fit(X_train, y_train, epochs=10, steps_per_epoch=10, validation_steps=1,validation_data=(X_test, y_test))
+"""
+#%%
+# Fit with auto-generated data
+model.fit_generator(
+        train_gen,
+        steps_per_epoch=train_gen.samples//BATCH_SIZE,
+        validation_steps=validation_gen.samples//BATCH_SIZE,
+        validation_data=validation_gen,
+        epochs=15
+        )
+model.save("custom_model_1.h5")
+#%%
+#NOT USED
+"""
+model.fit_generator(aug_gen.flow(X_train, y_train, batch_size=32), epochs=10, steps_per_epoch=25000//32, validation_steps=1,validation_data=(X_test, y_test))
+"""
